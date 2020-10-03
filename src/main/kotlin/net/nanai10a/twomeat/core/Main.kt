ad9f9535492f69fced38c76cbda518625e4c020b
Nanai10a <Nanai10a@tutanota.com>
2020-10-03 18:57:28 +0900

    [fix]changed launch to async because want to do await

--- src/main/kotlin/net/nanai10a/twomeat/core/Main.kt
+++ src/main/kotlin/net/nanai10a/twomeat/core/Main.kt
@@ -1,7 +1,7 @@
 package net.nanai10a.twomeat.core
 
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.async
 import kotlinx.coroutines.runBlocking
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.JDABuilder
@@ -12,9 +12,8 @@ import java.time.Instant
 import java.time.temporal.ChronoUnit
 
 
-fun main(args: Array<String>) {
-    runBlocking {
-        val bootedTime = Instant.now()
+fun main(args: Array<String>) = runBlocking {
+    val bootedTime = Instant.now()
 
     var isShutdown = false
     val logFile = File("2Meat-log-${Instant.now().toString().replace(":", "-")}.log")
@@ -22,63 +21,63 @@ fun main(args: Array<String>) {
     val stream = PrintStream(logFile)
     System.setErr(stream)
 
-        launch(Dispatchers.IO) {
-            while (!isShutdown)
-                stream.flush()
+    val flushDef = async(Dispatchers.IO) {
+        while (!isShutdown)
+            stream.flush()
+    }
+
+    val logger = LoggerFactory.getLogger("main()")
+    logger.info("booted 2Meat ver. 0.1.0-alpha")
+
+    logger.info("initialing...")
+
+    logger.debug("JDA instance building...")
+    val jda = JDABuilder
+        .createDefault(args[0])
+        .build()
+    var status: JDA.Status = jda.status
+
+    val logDef = async {
+        while (jda.status != JDA.Status.SHUTDOWN) {
+            if (status == jda.status)
+                continue
+            status = jda.status
+            logger.debug("JDA-instance's status had changed to ${status.name}")
+        }
+        logger.info("JDA had finished to shutdown")
+    }
+
+    jda.awaitReady()
+
+    logger.debug("JDA instance had built.")
+    logger.info("JDA (Java Discord API) ver. 4.2.0_207")
+
+    logger.debug("listener-loader init")
+    val loader = ListenerLoader()
+    logger.debug("listener-loader initialized")
+
+    logger.debug("listener-loader load")
+    loader.load()
+    logger.debug("listener-loader loaded")
+
+    loader.loadedListeners.let {
+        val listeners: List<Listener> = it ?: let {
+            logger.warn("listeners-list is null!")
+            ArrayList()
         }
 
-        val logger = LoggerFactory.getLogger("main()")
-        logger.info("booted 2Meat ver. 0.1.0-alpha")
-
-        logger.info("initialing...")
-
-        logger.debug("JDA instance building...")
-        val jda = JDABuilder
-            .createDefault(args[0])
-            .build()
-        runBlocking {
-            var status: JDA.Status = jda.status
-            launch {
-                while (jda.status == JDA.Status.SHUTDOWN) {
-                    if (status == jda.status)
-                        continue
-                    status = jda.status
-                    logger.debug("JDA-instance's status had changed to ${status.name}")
-                }
-                logger.info("JDA had finished to shutdown")
-            }
-
-            jda.awaitReady()
-
-            logger.debug("JDA instance had built.")
-            logger.info("JDA (Java Discord API) ver. 4.2.0_207")
-
-            logger.debug("listener-loader init")
-            val loader = ListenerLoader()
-            logger.debug("listener-loader initialized")
-
-            logger.debug("listener-loader load")
-            loader.load()
-            logger.debug("listener-loader loaded")
-
-            loader.loadedListeners.let {
-                val listeners: List<Listener> = it ?: let {
-                    logger.warn("listeners-list is null!")
-                    ArrayList()
-                }
-
-                var listenersNum = 0
-                logger.debug("listeners initialing...")
-                listeners.forEach { listener ->
-                    listenersNum++
-                    logger.debug("listener #$listenersNum [${listener.name}] is initialing...")
-                    listener.init()
-                    logger.debug("listener ${listener.name} ver. ${listener.version} initialized")
-                }
-                if (listenersNum == 0) logger.warn("listener was not initialized once!")
-                @Suppress("LocalVariableName")
-                val listener_ = if (listenersNum <= 1) "listeners" else "listener"
-                logger.debug("$listenersNum $listener_ initialized")
+        var listenersNum = 0
+        logger.debug("listeners initialing...")
+        listeners.forEach { listener ->
+            listenersNum++
+            logger.debug("listener #$listenersNum [${listener.name}] is initialing...")
+            listener.init()
+            logger.debug("listener ${listener.name} ver. ${listener.version} initialized")
+        }
+        if (listenersNum == 0) logger.warn("listener was not initialized once!")
+        @Suppress("LocalVariableName")
+        val listener_ = if (listenersNum <= 1) "listeners" else "listener"
+        logger.debug("$listenersNum $listener_ initialized")
 
         logger.debug("adding $listener_...")
         jda.addEventListener(*listeners.toTypedArray())
@@ -86,13 +85,15 @@ fun main(args: Array<String>) {
     }
     logger.info("initialized at ${bootedTime.until(Instant.now(), ChronoUnit.SECONDS)}sec")
 
-            logger.info("system started")
-        }
-        logger.info("started shutdown")
-        jda.shutdown()
-        logger.info("finished shutdown")
+    logger.info("system started")
 
-        logger.info("see you next time...")
-        isShutdown = true
-    }
+    logDef.await()
+
+    logger.info("started shutdown")
+    jda.shutdown()
+    logger.info("finished shutdown")
+
+    logger.info("see you next time...")
+    isShutdown = true
+    flushDef.await()
 }
