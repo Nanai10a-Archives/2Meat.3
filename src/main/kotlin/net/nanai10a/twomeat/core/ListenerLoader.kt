716e9a62226288456418534feaa7863b0ac6732a
Nanai10a <Nanai10a@tutanota.com>
2020-10-03 20:06:56 +0900

    [add]fun register(...) written

--- src/main/kotlin/net/nanai10a/twomeat/core/ListenerLoader.kt
+++ src/main/kotlin/net/nanai10a/twomeat/core/ListenerLoader.kt
@@ -1,11 +1,16 @@
 package net.nanai10a.twomeat.core
 
+import org.slf4j.LoggerFactory
 import java.util.concurrent.CopyOnWriteArrayList
 
 class ListenerLoader {
     companion object {
+        private val logger = LoggerFactory.getLogger(ListenerLoader::class.java)
         val listeners = CopyOnWriteArrayList<Listener>()
-        fun register(listener: Listener) {}
+        private fun register(listener: Listener) {
+            listeners.add(listener)
+            logger.debug("listener ${listener.name} was registered")
+        }
 
         fun load() {}
     }
