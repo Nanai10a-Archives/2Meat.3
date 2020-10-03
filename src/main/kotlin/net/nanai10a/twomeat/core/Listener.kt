cf3c344749f2e9c488e527b02c504efa56cb0e74
Nanai10a <Nanai10a@tutanota.com>
2020-10-03 23:18:10 +0900

    [add]TwoTJson and more written

--- src/main/kotlin/net/nanai10a/twomeat/core/Listener.kt
+++ src/main/kotlin/net/nanai10a/twomeat/core/Listener.kt
@@ -3,8 +3,8 @@ package net.nanai10a.twomeat.core
 import net.dv8tion.jda.api.hooks.ListenerAdapter
 
 abstract class Listener : ListenerAdapter() {
-    abstract val version: String
     abstract val name: String
+    abstract val version: String
 
     abstract fun init()
 }
